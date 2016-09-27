7754232cbcb89b5aa25f4df0559988bb7fa025b2
Alex <o0o.moro.o0o@gmail.com>
2016-09-27 03:26:40 -0400

    flexible delimiters, and fixed disconections messing up uart

--- arduino.lua
+++ arduino.lua
@@ -21,7 +21,7 @@ local function onInput(data)
 			disconnect=  false
 		end
 	end
-	if disconnect then uart.on("data") end
+	if disconnect then uart.on("data", 255, function() end, 0) end --don't return control. that might mess things up for connected devices.
 end
 local function setup(c,str)
 	local queue = serverconf.connections[c]
@@ -30,6 +30,7 @@ local function setup(c,str)
 
 	coroutine.resume(handlerInstance, queue)
 	table.insert(queue, handlerInstance)
-	uart.on("data", string.match(str, "!(.)"), onInput, 0)
+	local delim = tonumber(string.match(str, "!(%d+)")) or string.match(str, "!(.)") or 0
+	uart.on("data", delim, onInput, 0)
 end
 return  {pattern="!", func=firstTime, name='TCP passthrough', setup=setup}
